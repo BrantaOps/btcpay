70dda78dda4a6de447de6aaac06ae92a75a3d10f
Kyle McCullen <kmccullen@protonmail.com>
2025-10-08 10:32:29 -0400

    unit tests

---
+++ BTCPayServer.Plugins.Branta/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,8 @@
+﻿using BTCPayServer.Services.Invoices;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Plugins.Branta.Interfaces;
+public interface IInvoiceRepository
+{
+    Task<InvoiceEntity> GetInvoice(string invoiceId);
+}
